83d43923f4b5745e025abddd9467f056514cade4
gbayarri <genis.bayarri@irbbarcelona.org>
2024-12-16 09:42:23 +0100

    Deploy new build for irb

---
+++ irb/host-config.js
@@ -0,0 +1,51 @@
+// Import default query fields
+import defaultQueryFields from 'src/utils/constants/query-fields';
+
+// Set the default description
+const DEFAULT_DESCRIPTION = <>
+    <strong>MDposit</strong> is an open platform designed to provide web
+    access to atomistic molecular dynamics (MD) simulations. The aim of
+    this initiative is to ease and promote data sharing along the
+    wide-world scientific community in order to contribute in research.
+</>;
+
+// Set default values for every host config fields
+// These values are used to fill missing values
+const DEFAULT_HOST_CONFIGURATION = {
+    api: 'http://localhost:8081/rest/', // be aware that the URL must be the same as the one in the server (no relative paths allowed)
+    production: false,
+    name: 'MDposit',
+    favicon: 'mdposit_favicon',
+    description: DEFAULT_DESCRIPTION,
+    logo: 'logo-mdposit',
+    logoLabel: '',
+    primaryColor: '#808081', // Grey
+    secondaryColor: '#fafafa', // Light grey
+    searchExample: 'e.g. Orozco lab',
+    optionsField: undefined, // No browser selector and no data summary pie chart by default
+    optionsLabel: 'Options',
+    optionsNiceNames: {},
+    queryFields: defaultQueryFields,
+    options: []
+};
+
+// Set every host configuration
+const HOST_CONFIGURATIONS = {
+    // Testing
+    'localhost': {
+        api: 'https://irb.mddbr.eu/api/rest/', // be aware that the URL must be the same as the one in the server (no relative paths allowed)))
+        primaryColor: '#fb731e', // Orange,
+        logoLabel: 'IRB node',
+        name:'MDposit IRB'
+    }
+};
+
+// Set the current host configuration
+const HOST_CONFIG = HOST_CONFIGURATIONS['localhost'];
+
+// Fill the host configuration gaps with default values
+Object.entries(DEFAULT_HOST_CONFIGURATION).forEach(([ field, defaultValue ]) => {
+    if (!HOST_CONFIG[field]) HOST_CONFIG[field] = defaultValue;
+});
+
+export default HOST_CONFIG;
